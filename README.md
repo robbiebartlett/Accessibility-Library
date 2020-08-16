2500da10d049125c63c6b9a2b70a63b1d0747d10
Rob Bartlett <robbiebartlett@outlook.com>
2020-08-17 09:05:18 +1200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+MSD Design System
+
+This is the alpha stage for developing a design system to meet Government standards, WCAG 2.1 and Ministry of Social Development branding methodologies.
+
+An inclusive design approach is being undertaken where accessibility is integrated at the earliest phase of the design process to limit updates within testing.
+
+Currently there is Accessibility, Styles, Development Components and Design Patterns with there appropriate subsidiaries to come. The intention is to provide 
+direction for both our staff internally and our vendors to work towards MSD protocols.
+
+The framework is initially based off the All of Government (AoG) alpha release, references to gov.uk and communication with the Vendor working on the AoG release.
+This will create a minimal transition when AoG reaches production.
+
+Any questions please email Rob.Bartlett010@msd.govt.nz
---
+++ gulpfile.js
@@ -0,0 +1,179 @@
+// Initialize modules
+
+// Importing specific gulp API functions lets you write them below 
+// as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require('gulp');
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    svgSprite = require('gulp-svg-sprite'),
+    svgmin = require('gulp-svgmin'),
+	cheerio = require('gulp-cheerio'),
+    replace = require('gulp-replace'),
+    browserSync = require('browser-sync').create();
+
+
+    
+
+// File path variables
+const files = {
+    scssPath: 'src/scss/**/*.scss',
+    jsPath: 'src/js/**/*.js',
+    spritePath: 'src/images/**/*.svg'
+}
+
+
+
+
+// Sass task: 
+function scssTask(){    
+    return src(files.scssPath)
+        .pipe(sourcemaps.init()) 
+        .pipe(sass()) 
+        .pipe(postcss([ 
+            autoprefixer(), 
+            cssnano() 
+         ])) 
+        .pipe(sourcemaps.write('.')) 
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+          })
+    ); 
+}
+
+
+
+
+
+// JS task
+function jsTask() {
+    return src(files.jsPath)
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(dest('dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+          })
+
+    );
+}
+
+
+
+
+
+// SVG task
+function spriteTask() {
+    return src(files.spritePath)
+    // minify svg
+    .pipe(svgmin({
+        js2svg: {
+            pretty: true
+        }
+    }))
+
+    // remove all style and stroke declarations in out shapes
+		.pipe(cheerio({
+			run: function ($) {
+				 $('[stroke]').removeAttr('stroke');
+				 $('[style]').removeAttr('style');
+			},
+            parserOptions: {
+                xmlMode: true,
+            }
+        }))
+        // cheerio plugin creates unnecessary string '&gt;'
+        .pipe(replace('&gt;', '>'))
+        
+        // build svg sprite
+		.pipe(svgSprite({
+            "shape": {
+                "spacing": {
+                   "padding": 0,
+                   "box": 'content' 
+                },
+                "dest": "images/svgs"
+            },
+            "svg": {
+                "xmlDeclaration": false, 
+                "doctypeDeclaration": false,  
+                "namespaceIDs": false  
+            },
+            "mode": {
+                "symbol": {
+                    "dest": "images/sprites", 
+                    "sprite": "sprite.svg", 
+                    "inline": true, 
+                    "render": {
+                            "scss": {
+                               "dest":'../../../src/scss/tools/_sprites.scss', 
+                               "template":'src/scss/templates/_sprite_template.scss' 
+                            }
+                          },
+                    "example": true 
+                }
+            }
+		}))
+    .pipe(dest('dist'))
+    .pipe(browserSync.reload({
+        stream: true
+     })
+    ); 
+}
+
+ 
+
+
+
+
+// Caching task
+const cbString = new Date().getTime();
+function cacheTask(){
+    return src(['*.html'])
+        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))  
+        .pipe(dest('.')
+    );
+}
+
+
+
+// Watch task
+function watchTask(){
+    browserSync.init(null, {
+        "port": 3000,
+        "server": {
+        "baseDir": "."
+        },
+
+        "browser": [
+            "chrome",
+            "C:\\Program Files\\Firefox Developer Edition\\firefox.exe"
+        ]
+    });
+    watch([files.scssPath, files.jsPath],
+        series(
+            parallel(scssTask, jsTask),
+            cacheTask
+        )
+    );    
+}
+
+
+
+// Default task
+exports.default = series(
+    parallel(scssTask, jsTask, spriteTask), 
+    cacheTask,
+    watchTask
+);
+
+
+
+
+
+
