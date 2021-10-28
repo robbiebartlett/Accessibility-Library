4bb1bcce841c747e3270892f8662f2eb22f7cd93
Rob Bartlett <robbiebartlett@outlook.com>
2021-10-29 11:37:15 +1300

    Changes to accordion behaviour

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ function primaryNav () {
 
 
   //toggle sub-menu
-  $('.msd-navigation__toggle-level').on('click', function (e) {
+  $('.msd-navigation__mobile-toggle').on('click', function (e) {
   e.preventDefault();
 
 
@@ -43,7 +43,7 @@ function primaryNav () {
   //toggle submenu & ARIA values
   var openMenu=$(this).parent().toggleClass('active');    
   $(".msd-navigation__list-item").not(openMenu).removeClass('active');
-  $(".msd-navigation__toggle-level").not(this).attr("aria-expanded","false");
+  $(".msd-navigation__mobile-toggle").not(this).attr("aria-expanded","false");
 
 
 }); 
@@ -112,10 +112,11 @@ function backToTop() {
       featureDetected = true;
   }
 
-
+  var title = document.getElementById('skiplinks');
   $(".back-to-top").click(function () {
+    title.focus();
     $('html, body').animate({scrollTop: 0
-      }, 50).parent().focus();
+      }, 50).parent();
     return false;
   });
 
@@ -125,7 +126,9 @@ function backToTop() {
       onRepaint();
   });
 
-} // backToTop()
+
+
+} 
 
 
 
@@ -158,162 +161,35 @@ function timeStamp() {
 
 
 
+//Expand <details> accordion
+function accordion() {
 
-//Dropdown list
-function dropdown() {
-  'use strict';  
+  $('.msd-accordion__toggle').on('click', function (e) {
+    var $accordionGroups = $(".msd-accordion").find(".msd-accordion__details"),
+        $expandLink = $(".msd-accordion__toggle"),
+        $detailsOpen = $accordionGroups.prop("open"),
+        $detailsActive= $expandLink.hasClass("active");
 
-  var App = {};
+   if ($detailsActive == true) {
+     $accordionGroups.prop('open', false);
+     $expandLink.html("Expand All").toggleClass("active");
+   } else if (($detailsOpen == true) && ($detailsActive == false))  {
+     $accordionGroups.prop('open', true);
+     $expandLink.html("Collapse All").toggleClass("active");
+   } else  {
+     $accordionGroups.prop('open', true);
+     $expandLink.html("Collapse All").toggleClass("active");
+   }
 
-  App.buttonCount = 0;
-
-  // Panel 
-  App.panel = document.querySelectorAll('[data-js=msd-filter__panel]');
-
-  [].forEach.call( App.panel, function(el) {
-    el.setAttribute('role', 'tablist');
-    el.setAttribute('aria-multiselectable', 'true');
-  });
-
-  // Section var
-  App.section = document.querySelectorAll('[data-js=msd-filter__section]');
-
-  // Each seciton
-  // Inspired by http://jsfiddle.net/toddmotto/KjnG2/2/
-  [].forEach.call( App.section, function(el, i) {   
-
-    // Heading var, hide the heading
-    App.heading = el.querySelector('[data-js=msd-filter__heading]');
-    App.heading.classList.add('msd-filter__hidden');     
-
-
-    // Content var, set ARIA property
-    App.content = el.querySelector('[data-js=msd-filter__content]');    
-    App.content.setAttribute('aria-hidden', 'true');
-    App.content.setAttribute('aria-labelledby', App.heading.id);
-    App.content.setAttribute('role', 'tabpanel');
-
-    // Create the button
-    App.button = document.createElement('button');
-
-    // Assign class, and attributes
-    App.button.id = App.heading.id;
-    App.button.classList.add('msd-filter__button'); 
-    App.button.setAttribute('data-js', 'button');
-    App.button.innerHTML = App.heading.innerHTML;
-    App.button.setAttribute('aria-expanded', 'false');
-    App.button.setAttribute('aria-selected', 'false');
-    App.button.setAttribute('aria-controls', App.content.id);
-    App.button.setAttribute('role', 'tab');
-    App.button.setAttribute('tabindex', '-1');
-
-    // Insert the button 
-    App.heading.parentNode.insertBefore(App.button, App.heading);
-
-    // Button count, used in keyboard nav
-    App.buttonCount++;
-
-    // Button click event handler
-    App.button.onclick = function () {
-
-      var thisButton = this,
-          thisContent = document.querySelectorAll('[data-js=msd-filter__content]');
-
-      // Toogle active state (+/-)
-      thisButton.classList.toggle('msd-filter__button--active');
-
-      // Toggle ARIA property
-      // https://gist.github.com/toddmotto/bbb704d88cf39b06dbe0
-      thisButton.setAttribute('aria-expanded', thisButton.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
-
-      // Toogle active state (show/hide)
-      thisContent[i].classList.toggle('msd-filter__content--active'); 
-
-      // Toggle ARIA property    
-      thisContent[i].setAttribute('aria-hidden', App.content.getAttribute('aria-hidden') === 'true' ? 'false' : 'true');
-
-    };
-
-    // Keyboard navigation
-    // http://www.w3.org/TR/2013/WD-wai-aria-practices-20130307/#accordion
-    App.button.onfocus = function () {
-
-      var thisButton = this;       
-
-      // Set selected state
-      [].forEach.call( document.querySelectorAll('[data-js=button]'), function(el) { 
-        el.setAttribute('aria-selected', 'false');
-        el.setAttribute('tabindex', '-1');
-      });
-      thisButton.setAttribute('aria-selected', 'true');
-      thisButton.setAttribute('tabindex', '0');
-
-    };
-
-    App.button.onkeydown = function (e) {
-
-      var thisButton = this;          
-
-      switch(e.which) {
-        // Left/Up
-        case 37:
-        case 38:
-
-          e.preventDefault();
-          // Check for previous node
-
-          if (!thisButton.parentNode.previousElementSibling) {
-            // No previous node,
-            // Set focus on last node
-            thisButton.parentNode.parentNode.getElementsByTagName('section')[App.buttonCount-1].getElementsByTagName('button')[0].focus();
-          } else {
-            // Move focus to previous
-            thisButton.parentNode.previousElementSibling.getElementsByTagName('button')[0].focus();
-          }
-
-          break;
-
-        // Right/Down
-        case 39:
-        case 40:
-          e.preventDefault();
-
-          // Check for next node
-
-          if (!thisButton.parentNode.nextElementSibling) {
-            // No next node,
-            // Set focus on first node            
-            thisButton.parentNode.parentNode.getElementsByTagName('section')[0].getElementsByTagName('button')[0].focus();
-          } else {
-            // Move focus to next
-            thisButton.parentNode.nextElementSibling.getElementsByTagName('button')[0].focus();
-          }
-          break;
-
-        // Home
-        case 36:          
-            // Set focus on first node
-          thisButton.parentNode.parentNode.getElementsByTagName('section')[0].getElementsByTagName('button')[0].focus();
-          break;
-
-        // End
-        case 35:          
-            // Set focus on last node
-          thisButton.parentNode.parentNode.getElementsByTagName('section')[App.buttonCount-1].getElementsByTagName('button')[0].focus();
-          break;
-
-      }
-
-    }; 
-
-    // Remove heading from DOM
-    el.removeChild(App.heading);
+     //Set ARIA values
+  var openAccordion = $(this);
+  if (openAccordion.attr("aria-expanded") === "false") {
+    openAccordion.attr("aria-expanded", "true");
+  } else {
+    openAccordion.attr("aria-expanded", "false");
+  }
 
   });
-
-  // Make first button selectable
-  document.querySelector('[data-js=button]').setAttribute('tabindex', '0');
-  document.querySelector('[data-js=button]').setAttribute('aria-selected', 'true');
 
 }
 
@@ -321,23 +197,33 @@ function dropdown() {
 
 
 
-//Re-order search filter in mobile
-function filterReorder() {
 
+//Filter list
+function filter() {
 
-$(window).on("load resize", function () {
+  //Used to minify filters in mobile
+  $(window).on("load resize", function () {
 
-   if ($(window).width() <= 770) {
+    if ($(window).width() <= 770) {
+      //Set filter to closed
+      document.getElementById("msd-filter").open = false;
+      //Add focus element
+      $(".msd-filter__summary").attr('tabindex', '1');
+      //Change tab order in mobile view
       $('.msd-order-1').insertBefore('.msd-order-2');
+ 
+       }  else {
+ 
+        document.getElementById("msd-filter").open = true;
+        $(".msd-filter__summary").attr('tabindex', '-1');
+        $('.msd-order-2').insertBefore('.msd-order-1');
+     }
 
-      }  else {
-
-       $('.msd-order-2').insertBefore('.msd-order-1');
-    }
-
-   });
-
+   }); 
 }
+
+
+
 
 
 
@@ -349,6 +235,6 @@ $(document).ready(function () {
   backToTop();
   printPage();
   timeStamp();
-  dropdown();
-  filterReorder();
+  accordion();
+  filter();
 });
